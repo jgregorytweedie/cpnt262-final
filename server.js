8fa383d8dba1953760ebeec04e56f148d645722b
Ivan Lavena <lavenaivan@gmail.com>
2020-11-16 00:18:20 -0700

    installed 'dotenv' + modified 'server.js'

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 // these are all our dependencies that we need for the project - jayden.
-const app = require("express");
-const view = require("ejs");
-const mongoDB = require("mongoose");
+const path = require('path')
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
 require("dotenv").config();
+
+// CREATE EXPRESS APP
+const app = express();
+app.set('view engine', 'ejs');
+
+// app.use STATIC MIDDLEWARE
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ADD JSON ENDPOINTS
+
+// 404 RESPONSE
+app.use(function(require, response, next){
+  response.status(404);
+  response.send('404: File Not Found');
+});
+
+const PORT = process.env.PORT || 8080;
+
+// START SERVER
+app.listen(PORT, function(){
+  console.log(`listening on port ${PORT}`);
+});
