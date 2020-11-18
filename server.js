efe617a17bf8a2ca31c073cc2b4ecee0dca6937e
jgregorytweedie <71092974+jgregorytweedie@users.noreply.github.com>
2020-11-17 22:50:06 -0700

    added in dotenv file, and added in mongoose support

--- server.js
+++ server.js
@@ -2,9 +2,20 @@
 const path = require('path')
 const express = require("express");
 const ejs = require("ejs");
+// mongoose stuff here - jayden
 const mongoose = require("mongoose");
-require("dotenv").config();
+const dotenv = require("dotenv").config();
+// adding in connection to mongoDB - jayden
+mongoose.connect(process.env.MONGODB_URL, { useUnifiedTopology: true, useNewUrlParser: true });
 
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+db.once("open", function() {
+  console.log("Connected to DB...");
+
+});
 // CREATE EXPRESS APP
 const app = express();
 app.set('view engine', 'ejs');
