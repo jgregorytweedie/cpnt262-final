b717a2fe50f3904316683dd397c2e18534817cd8
SANAZDANESHVAR <70992876+SANAZDANESHVAR@users.noreply.github.com>
2020-11-19 10:30:42 -0700

    Add files via upload

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,77 +1,32 @@
-fetch (`${window.location.origin}/api/v0/gallery`)
-  .then(function(response){
-    // json returned from server
+const main =
+// Asynchronous fetch gallery
+fetch(`${window.location.origin}/api/v0/gallery`)
+
+  // send JSON fetch
+  .then(function (response) {
     return response.json();
-})
-  .then(function(shoes){
-  // data Javascript object 
-  console.log(shoes);
+  })
 
-  let output = '';
+  .then(function (shoes) {
+    console.log(shoes);
+    let output = '';
 
-  shoes.forEach(function(shoes) {
-    output += `<figure class="card">
-                  <img src="/images/gallery/${shoes.id}.jpg" alt="${shoes.title}"
-                  width="${shoes.width}" height="${shoes.height}">
+    cakes.forEach(function (shoes) {
+      output += `<figure class="card">
+                  <img src=${shoes.id} alt="${shoes.title}" width=>
                   <figcaption>
-                    <h5>${shoes.description}</h5>
+                    <p>${shoes.description}</h2>
                   </figcaption>
                 </figure>`;
+    });
+
+    // output to DOM
+    document.querySelector('.gallery').innerHTML = output;
+  })
+
+  // error 
+  .catch(function (error) {
+    if (error) {
+      console.log(" NO INFORMATION!");
+    }
   });
-
-  // container for images
-  document.querySelector('.gallery').innerHTML = output;
-})
-
-.catch(function(error){
-  if (error) {
-    console.log ("Oh no! you've done something wrong");
-  }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// fetch('http://localhost:3000/api/v0/gallery')
-//   .then(function(response){
-//     // JSON `data` returned from server
-//     // We need to convert it in a Javascript object
-//     return response.json();
-//   })
-//   .then(function(shoes){
-//     // `data`Javavascript object 
-//     console.log(shoes);
-
-//     let outputHTML = '';
-
-//     shoes.forEach(function(shoes){
-//       outputHTML += `
-//         <figure>
-//           <img src="../images/gallery/id/${shoes}" alt="${shoes.title}">
-//           <figcaption>"${shoes.description}"
-//         </figure>`;
-//     })
-
-//     // output to DOM
-//     document.querySelector('.gallery').innerHTML = outputHTML;
-
-//   })
-//   .catch(function(error){
-//     if (error) {
-//       console.log('Oh Noooooooos!');
-//     }
-//   });
