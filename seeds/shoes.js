0ffdcc8d40d4cd0ceeaedfac3fa5cf5d7792db10
Ivan Lavena <lavenaivan@gmail.com>
2020-11-18 22:43:17 -0700

    C:/Program Files/Git/seeds added

---
+++ seeds/shoes.js
@@ -0,0 +1,85 @@
+const shoes = [
+  {
+    id:           1011,
+    title:        'some shoes',
+    description:  'some description',
+    width:        500,
+    height:       500,
+    imagePath:    'seeds/shoes.js',
+
+  },
+  {
+    id:           1012,
+    title:        'some shoes',
+    description:  'some description',
+    width:        500,
+    height:       500,
+    imagePath:    'seeds/shoes.js',
+
+  },
+  {
+    id:           1013,
+    title:        'some shoes',
+    description:  'some description',
+    width:        500,
+    height:       500,
+    imagePath:    'seeds/shoes.js',
+
+  },
+  {
+    id:           1014,
+    title:        'some shoes',
+    description:  'some description',
+    width:        500,
+    height:       500,
+    imagePath:    'seeds/shoes.js',
+
+  },
+  {
+    id:           1015,
+    title:        'some shoes',
+    description:  'some description',
+    width:        500,
+    height:       500,
+    imagePath:    'seeds/shoes.js',
+
+  },
+  {
+    id:           1016,
+    title:        'some shoes',
+    description:  'some description',
+    width:        500,
+    height:       500,
+    imagePath:    'seeds/shoes.js',
+
+  },
+  {
+    id:           1017,
+    title:        'some shoes',
+    description:  'some description',
+    width:        500,
+    height:       500,
+    imagePath:    'seeds/shoes.js',
+
+  },
+  {
+    id:           1018,
+    title:        'some shoes',
+    description:  'some description',
+    width:        500,
+    height:       500,
+    imagePath:    'seeds/shoes.js',
+
+  },
+  {
+    id:           1019,
+    title:        'some shoes',
+    description:  'some description',
+    width:        500,
+    height:       500,
+    imagePath:    'seeds/shoes.js',
+
+  },
+]
+
+module.exports = shoes;
