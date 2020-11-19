b513a5f27ba7a7443864377d89fbfdeff7067d43
Ivan Lavena <lavenaivan@gmail.com>
2020-11-19 14:59:54 -0700

    index endpoint fixed

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // ADD JSON ENDPOINTS
 
 // index endpoint
-app.get('/home', function(request, response){
+app.get('/', function(request, response){
   response.render('pages/index.ejs', {title: 'title'})
 });
 
