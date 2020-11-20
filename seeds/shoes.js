34fbae619f9af53e2e9fed425afc9363f32dcae3
jgregorytweedie <71092974+jgregorytweedie@users.noreply.github.com>
2020-11-20 13:05:03 -0700

    updated shoes.js

--- seeds/shoes.js
+++ seeds/shoes.js
@@ -2,7 +2,7 @@ const shoes = [
   {
     id:           1011,
     title:        'some shoes',
-    description:  'first pair of shoes',
+    description:  'Nike',
     width:        500,
     height:       500,
     imagePath:    'seeds/shoes.js',
@@ -11,7 +11,7 @@ const shoes = [
   {
     id:           1012,
     title:        'some shoes',
-    description:  'second pair of shoes',
+    description:  'black Nike',
     width:        500,
     height:       500,
     imagePath:    'seeds/shoes.js',
@@ -20,7 +20,7 @@ const shoes = [
   {
     id:           1013,
     title:        'some shoes',
-    description:  'third pair of shoes',
+    description:  'Colorful shoes',
     width:        500,
     height:       500,
     imagePath:    'seeds/shoes.js',
@@ -29,7 +29,7 @@ const shoes = [
   {
     id:           1014,
     title:        'some shoes',
-    description:  'fourth pair of shoes',
+    description:  'white fashionable Nike',
     width:        500,
     height:       500,
     imagePath:    'seeds/shoes.js',
@@ -38,7 +38,7 @@ const shoes = [
   {
     id:           1015,
     title:        'some shoes',
-    description:  'fifth pair of shoes',
+    description:  'White basketball Nike',
     width:        500,
     height:       500,
     imagePath:    'seeds/shoes.js',
@@ -47,7 +47,7 @@ const shoes = [
   {
     id:           1016,
     title:        'some shoes',
-    description:  'sixth pair of shoes',
+    description:  'athletic light black shoes',
     width:        500,
     height:       500,
     imagePath:    'seeds/shoes.js',
@@ -56,7 +56,7 @@ const shoes = [
   {
     id:           1017,
     title:        'some shoes',
-    description:  'seventh pair of shoes',
+    description:  'skateboarder Nike',
     width:        500,
     height:       500,
     imagePath:    'seeds/shoes.js',
@@ -65,7 +65,7 @@ const shoes = [
   {
     id:           1018,
     title:        'some shoes',
-    description:  'eigth pair of shoes',
+    description:  'Vans',
     width:        500,
     height:       500,
     imagePath:    'seeds/shoes.js',
@@ -74,7 +74,7 @@ const shoes = [
   {
     id:           1019,
     title:        'some shoes',
-    description:  'nineth pair of shoes',
+    description:  'Vans-style Nike,
     width:        500,
     height:       500,
     imagePath:    'seeds/shoes.js',
